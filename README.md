d5ab7dc4349333af2ad40ccc4857daa063396d58
sungmin <smoh2044@gmail.com>
2016-10-13 23:55:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## 서울대학교 관악캠퍼스 식단표
+
+서울대학교 관악캠퍼스의 식단표를 보기 위한 Alfred workflow 입니다.
+
+### 사용법
+
+`menu` + `<m / l / d>` + `<number>`
+
+- `number` : 오늘로부터의 몇번째 날. 기본값은 0 (오늘)
+- `m/l/d `: 순서대로 아침, 점심, 저녁. 값을 주지 않으면 전체 식단
+- 파라메터의 순서는 바뀌어도 괜찮습니다.
+
+**솔밭식당, 샤반, 공대간이식당, 연건식당, 소담마루, 라운지오의 식단은 제외합니다.**
+
+
+
+-------------
+
+
+
+## Seoul National University Cafeteria Menu (Gwanak)
+
+This is an alfred workflow for checking cafeteria menu of SNU(Gwanak campus).
+
+#### Usage
+
+`menu` + `<m / l / d>` + `<number>`
+
+- `number` : Which date from today. Default value is 0 (means today).
+
+- `m/l/d `: Breakfast, lunch, dinner in order. You can see all menu without this parameter.
+
+- The order of parameters does not matter. 
+
+**Solbat, Shaban, Gonggang, Yeongun, Sodam maru, Launge-o are excluded.**
---
+++ snumenu.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+import urllib2
+import sys
+# sys.path.append('/usr/local/lib/python2.7/site-packages')
+from lxml import html
+from datetime import date, timedelta
+
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except:
+        return False
+
+t = None
+q = None
+
+try:
+    queries = sys.argv[1:]
+    if is_int(queries[0]):
+        t = queries[0]
+        q = queries[1]
+    else:
+        q = queries[0]
+        t = queries[1]
+except:
+    pass
+
+url1 = 'http://www.snuco.com/html/restaurant/restaurant_menu1.asp'
+url2 = 'http://www.snuco.com/html/restaurant/restaurant_menu2.asp'
+if t:
+    try:
+        date = (date.today() + timedelta(days=int(t))).strftime('%Y-%m-%d')
+        url1 = url1+('?date=%s'%date)
+        url2 = url2+('?date=%s'%date)
+    except:
+        pass
+
+req1 = urllib2.Request(url1)
+req2 = urllib2.Request(url2)
+response1 = urllib2.urlopen(req1)
+response2 = urllib2.urlopen(req2)
+page1 = response1.read()
+page2 = response2.read()
+
+def print_dic(dic):
+    for key, value in dic.iteritems():
+        print key
+        print value
+
+items = []
+
+for cafe in range(2):
+    if cafe == 0:
+        cafeterias = html.fromstring(page1).xpath("//span[@class='left_text14']")
+    else:
+        cafeterias = html.fromstring(page2).xpath("//span[@class='left_text14']")
+
+    for i in range(len(cafeterias)):
+        if (cafe == 0 and i == 5) or (cafe == 1 and i in [4,5,6,7,9]): continue
+
+        parent = cafeterias[i].getparent().getparent()
+        cafeteria = cafeterias[i].text_content()
+        breakfast = 'M:' + parent.getchildren()[2].text_content().replace('\n', '/')
+        lunch = 'L:' + parent.getchildren()[4].text_content().replace('\n>', '/')
+        dinner = 'D:' + parent.getchildren()[6].text_content().replace('\n', '/')
+        if q == 'm':
+            menu = breakfast
+        elif q == 'l':
+            menu = lunch
+        elif q == 'd':
+            menu = dinner
+        else:
+            menu = breakfast + lunch + dinner
+
+        items.append('<item><title>%s</title><subtitle>%s</subtitle></item>'\
+                %(cafeteria, menu))
+
+
+print '<items>'
+for item in items:
+    print item.encode('utf-8').replace('&', '&amp;')
+print '</items>'
+
